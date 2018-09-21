77bc52582f69071b790244f46b4ed044473228cf
Chris <christopherjbbaker@gmail.com>
2018-09-20 20:54:11 -0400

    webcam additions

--- FMF_client/client.js
+++ FMF_client/client.js
@@ -1,6 +1,20 @@
 var io = require('socket.io-client');
+var nodeWebcam = require('node-webcam');
 var HOST = '127.0.0.1';
 var PORT = 3000;
+
+// Webcam default options
+var opts = {
+    width: 1280,
+    height: 720,
+    quality: 100,
+    delay: 0,
+    saveShots: true,
+    output: "jpeg",
+    device: false,
+    callbackReturn: "location",
+    verbose: true
+};
 
 /* Package these variables in exe */
 const TOKEN = '001';
@@ -11,8 +25,18 @@ var socket = io.connect('http://' + HOST + ':' + PORT + '/dashboard',
     {query: {token: TOKEN, user: USER}});
 
 socket.on('connect', function () {
-    socket.on('message', function(data) {
-        console.log(data);
+    socket.on('take_webcam_picture', function(data) {
+        // Create webcam instance 
+        var webcam = nodeWebcam.create(opts);
+
+        webcam.capture('.pic', function(error, data) {console.log(data);});
+
+        //var fileReader = new FileReader(), slice = File.slice(0, 100000);
+        
+        
+        
+        webcam.clear();
+
     });
     
     //emit password and/or appId over TLS to prove its truly the right node
